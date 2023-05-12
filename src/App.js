01b15644aee72fe0c689d488381784819b06a338
Jackie <irene.yanapouth@gmail.com>
2023-05-12 10:32:02 +0200

    third

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>Hello World</div>
+    <div className='container'>
+        <h1>Hello World</h1>
+        <div>Hello toi</div>
+
+        <button>Ici</button>
+        <button> oui oui Ici</button>
+        <button>t'inquiéte on ne vas pas te manger</button>
+    </div>
+
   )
 }
 
